c474b039f7b9dc6f9ad5f2b52826c805dd69c841
Aria_250602 <ariapratama.2002@gmail.com>
2021-09-02 15:05:21 +0700

    update materi

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ const tagbr = document.querySelectorAll("span.tagbr");
 tagbr.forEach((tagbrsatuan) => {
     tagbrsatuan.innerHTML = "&#60;br&#62;";
 });
+const taghr = document.querySelectorAll("span.taghr");
+taghr.forEach((taghrsatuan) => {
+    taghrsatuan.innerHTML = "&#60;hr&#62;";
+});
+const tagpbuka = document.querySelectorAll("span.tagpbuka");
+tagpbuka.forEach((tagpbukasatuan) => {
+    tagpbukasatuan.innerHTML = "&#60;p&#62;";
+});
+const tagptutup = document.querySelectorAll("span.tagptutup");
+tagptutup.forEach((tagptutupsatuan) => {
+    tagptutupsatuan.innerHTML = "&#60;/p&#62;";
+});
+const tagboldbuka = document.querySelectorAll("span.tagboldbuka");
+tagboldbuka.forEach((tagboldbukasatuan) => {
+    tagboldbukasatuan.innerHTML = "&#60;b&#62;";
+});
+const tagboldtutup = document.querySelectorAll("span.tagboldtutup");
+tagboldtutup.forEach((tagboldtutupsatuan) => {
+    tagboldtutupsatuan.innerHTML = "&#60;/b&#62;";
+});
+const tagitalicbuka = document.querySelectorAll("span.tagitalicbuka");
+tagitalicbuka.forEach((tagitalicbukasatuan) => {
+    tagitalicbukasatuan.innerHTML = "&#60;i&#62;";
+});
+const tagitalictutup = document.querySelectorAll("span.tagitalictutup");
+tagitalictutup.forEach((tagitalictutupsatuan) => {
+    tagitalictutupsatuan.innerHTML = "&#60;/i&#62;";
+});
+const tagunderlinebuka = document.querySelectorAll("span.tagunderlinebuka");
+tagunderlinebuka.forEach((tagunderlinebukasatuan) => {
+    tagunderlinebukasatuan.innerHTML = "&#60;u&#62;";
+});
+const tagunderlinetutup = document.querySelectorAll("span.tagunderlinetutup");
+tagunderlinetutup.forEach((tagunderlinetutupsatuan) => {
+    tagunderlinetutupsatuan.innerHTML = "&#60;/u&#62;";
+});
+const tagprebuka = document.querySelectorAll("span.tagprebuka");
+tagprebuka.forEach((tagprebukasatuan) => {
+    tagprebukasatuan.innerHTML = "&#60;pre&#62;";
+});
+const tagpretutup = document.querySelectorAll("span.tagpretutup");
+tagpretutup.forEach((tagpretutupsatuan) => {
+    tagpretutupsatuan.innerHTML = "&#60;/u&#62;";
+});
